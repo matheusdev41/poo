90c141cb8bfc30697e93bb25afdfe7c0ceb856b4
Matheus Pessoa <matheus41.dev@gmail.com>
2025-11-22 17:52:43 -0300

    aula2

---
+++ Main.py
@@ -0,0 +1,27 @@
+from Pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('João', 32)
+
+p1.get_ano_nascimento()
+p2.get_ano_nascimento()
+
+# print(p1.ano_atual)
+# print(p2.ano_atual)
+# print(Pessoa.ano_atual)
+
+
+# p1.falar('poo')
+# p2.comer('sorvete')
+# p1.comer('Churrasco')
+
+
+# p1.comer('maçã')
+# p1.falar('poo')
+# p1.parar_comer()
+# p1.falar('poo')
+# p1.comer('alimento')
+# p1.parar_falar()
+# p1.falar('assunto')
+
+
